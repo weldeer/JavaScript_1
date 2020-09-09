dff6c38c3ec94f0b295a79e73090cf5dfcfb7948
jakovlev_m <jakovlev_m@mail.ru>
2020-09-09 17:03:35 +0300

    new files

--- script.js
+++ script.js
@@ -3,13 +3,40 @@
 // document.onload = function () {
 //     run()
 // }
-document.addEventListener("DomContentLoaded", ready);
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("1243")
+    // //Обычно этот как в CSS селекторе
+    // const arrP = document.querySelectorAll('p');
+    // const twoP = arrP[1];
+    // twoP.innerHTML = '999';
+    // twoP.style.color = 'red';
+    // console.log(console.log(arrP));
+    // //twoP.remove();
+    //
+    // //создание ноды
+    // const h1 = document.createElement('h1')
+    // h1.innerHTML = 'ВЕДМЕД';
+    // twoP.before(h1); // можно вставлять перед после в конец в начало внутри тега
 
 
+    setTimeout(function () {
+        const container = document.querySelectorAll('div')[0];
+        const h2 = document.createElement('h2');
+        h2.innerHTML = "динамический заголовок";
+        //container.after(h2); // после
+        //container.before(h2);
+        //container.append(h2); // внутри вконце
+        container.prepend(h2);  // внутри вначале
+        console.log('4444')
+    }, 2000)
+})
 
-// НИкогда проактически не работает
+
+function test() {
+    //alert('bplf');
+    console.log('dscsxs')
+}
+
+// НИкогда проактически не используется
 //const arrP = document.getElementsByName('p');
 
-//Обычно этот как в CSS селекторе
-const arrP = document.querySelector('p');
-console.log(arrP);
